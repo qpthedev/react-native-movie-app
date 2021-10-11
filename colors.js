991cff346f19074ccd36be73fd473d37eab65829
qpthedev <qpthedev@gmail.com>
2021-10-11 18:39:03 -0400

    Bottom Tabs Navigator and Icons

---
+++ colors.js
@@ -0,0 +1,4 @@
+export const kYELLOW = "#ffc048";
+export const kBLACK = "#1e272e";
+export const kDARK_GRAY = "#808e9b";
+export const kLIGHT_GRAY = "#d2dae2";
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,15 +3,65 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/TV";
 import Search from "../screens/Search";
+import { useColorScheme } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { kBLACK, kDARK_GRAY, kLIGHT_GRAY, kYELLOW } from "../colors";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} />
-    <Tab.Screen name="TV" component={Tv} />
-    <Tab.Screen name="Search" component={Search} />
-  </Tab.Navigator>
-);
+const Tabs = () => {
+  const isDark = useColorScheme() === "dark";
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: isDark ? kBLACK : "white",
+        },
+        tabBarActiveTintColor: isDark ? kYELLOW : kBLACK,
+        tabBarInactiveTintColor: isDark ? kDARK_GRAY : kLIGHT_GRAY,
+        headerStyle: {
+          backgroundColor: isDark ? kBLACK : "white",
+        },
+        headerTitleStyle: {
+          color: isDark ? "white" : kBLACK,
+        },
+        tabBarLabelStyle: {
+          marginTop: -5,
+          fontSize: 12,
+          fontWeight: "700",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="film-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="TV"
+        component={Tv}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="tv-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search-outline" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
